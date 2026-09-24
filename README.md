6dc4bab5ed3527af5f67d53b0f1194441f8befee
Kudrat Jp <jp.kudrat464@gmail.com>
2026-09-24 20:58:25 +0900

    Initial JAVA learnign project

---
+++ README.md
@@ -0,0 +1,42 @@
+\# Java Learning
+
+
+
+This repository contains my Java learning and practice programs.
+
+
+
+\## Topics
+
+
+
+\- Java Basics
+
+\- Variables
+
+\- Conditional Statements
+
+\- Loops
+
+\- Arrays
+
+\- Methods
+
+\- Object-Oriented Programming
+
+\- Collections
+
+\- Exception Handling
+
+\- Java Projects
+
+
+
+\## Environment
+
+
+
+\- Java
+
+\- Eclipse IDE
+
---
+++ src/pkg1/gpt1.java
@@ -0,0 +1,46 @@
+package pkg1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class gpt1 {
+	public static void main(String[] args) {
+//		String name = "kudrat";
+//		int age = 25;
+//		
+//		System.out.println(name);
+//		
+//		if(age >= 18) {
+//			System.out.println("Adult");
+//		}else {
+//			System.out.println("Minor");
+//		}
+		
+//		--for loop
+//		for (int i = 0; i < 5; i++) {
+//			System.out.println(i);
+//		}
+		
+//		--enhanced loop
+//String[] names = {"kudrat", "rana", "rifat"};
+//		
+//		for(String name : names) {
+//			System.out.println(name);
+//		}
+		
+//		--string
+//		String[] names = {"kudrat","rahim", "rana"};
+//		System.out.println(names[0]);
+//	}
+//}
+		
+		List<String> names = new ArrayList<>();
+		
+		names.add("kudrat");
+		names.add("rahim");
+		names.add("karim");
+		
+		System.out.println(names.get(1));
+	}
+}
