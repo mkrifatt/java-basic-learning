1ba568d056d52a2cc49d009a6f659a29ad11ce2e
Kudrat Jp <jp.kudrat464@gmail.com>
2026-09-26 10:14:26 +0900

    METHOD IN JAVA

--- src/pkg1/gpt1.java
+++ src/pkg1/gpt1.java
@@ -5,43 +5,81 @@ import java.util.List;
 
 
 public class gpt1 {
+//	--FOllOW 7 UNDER METHOD
+	public int add(int int1, int int2) {
+		return int1 + int2;
+	}
+//	<this is a method that returns a string value and it is not inside the main method, 
+//	it can be called from the main method
+//	by creating an object of the class and then calling the method using that object.>
+	public String getName() {
+		return "kudrat";
+	}
+	public void greet() {
+		System.out.println("Hello, welcome to the java program!");
+	}
 	public static void main(String[] args) {
+//		--------------------------
+//		1--BASIC
 //		String name = "kudrat";
 //		int age = 25;
 //		
 //		System.out.println(name);
 		
-//		--condition
+		
+//		--------------------------
+//		2--CONDITION
 //		if(age >= 18) {
 //			System.out.println("Adult");
 //		}else {
 //			System.out.println("Minor");
 //		}
 		
-//		--for loop
+//		3--FOR LOOP
 //		for (int i = 0; i < 5; i++) {
 //			System.out.println(i);
 //		}
 		
-//		--enhanced loop
-//String[] names = {"kudrat", "rana", "rifat"};
+		
+//		-----------------------------------
+//		4--ENCHSED FOR LOOP
+//		String[] names = {"kudrat", "rana", "rifat"};
 //		
 //		for(String name : names) {
 //			System.out.println(name);
 //		}
 		
-//		--string
+		
+		
+//		5--STRING ARRAY
 //		String[] names = {"kudrat","rahim", "rana"};
 //		System.out.println(names[0]);
 //	}
-//}
-//		--LIST STRING
+		
+		
+//}		-----------------------------------
+//		6--LIST STRING
 //		List<String> names = new ArrayList<>();
 //		
 //		names.add("kudrat");
 //		names.add("rahim");
 //		names.add("karim");
 //		
+		
+		
 //		System.out.println(names.get(1));
+//		-------------------------
+//		7--METHOD
+//		<creating an object of the gpt1 class beacause the add method is not static but 
+//		if it is static then we can call it directly without creating an object
+//		as a example, if the add method is static then we can call it like this: gpt1.add(5, 10);>
+		
+//		gpt1 demo = new gpt1();
+//		int sum = demo.add(5, 10);
+//		System.out.println("Sum: " + sum);
+//		
+//		String name = demo.getName();
+//		System.out.println("Name: " + name);
+//		demo.greet();
 	}
 }
